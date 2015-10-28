9e02de895f1c4e122ca02df66169f3595fb5dd8e
Siebe <vandijckske@gmail.com>
2015-10-28 22:06:46 +0000

    cleaning up code

--- js/app.js
+++ js/app.js
@@ -1,21 +1,17 @@
 var ticking = false,
-    lastMousePosition = { x: 0, y: 0 },
     scrollPosition = 0,
-    center = {x: 0, y: 0},
     moveEls = document.querySelectorAll('.js-parallax'),
-    moveElsLength = moveEls.length,
-    steps = ['images','videos','docs'];
+    moveElsLength = moveEls.length;
 
-getScreenCenter();
 calculateBg();
 
 
 // Only on mobile
-if (window.innerWidth > 760){
+if (window.innerWidth > 760) {
     document.addEventListener('scroll', MoveHandler);
     window.addEventListener('resize', calculateBg);
-
 }
+
 
 function calculateBg() {
     var w = window.innerWidth + 100,
@@ -24,33 +20,24 @@ function calculateBg() {
         scaleH = h/744,
         newScale = Math.max(scaleW, scaleH);
 
-        if(newScale < 1) {
-            newScale = 1;
-        }
+    if (newScale < 1) {
+        newScale = 1;
+    }
 
-
-        document.querySelector('.bg').style.transform = 'scale('+ newScale  + ')';
-
-
+    document.querySelector('.landing__bg').style.transform = 'scale('+ newScale  + ')';
 }
 
-
-function getScreenCenter() {
-
-    center.x = window.innerWidth/2;
-    center.y = window.innerHeight/2;
-}
 
 function MoveHandler(e) {
     scrollPosition = window.scrollY;
-    //lastMousePosition.y = e.clientY;
     requestTick();
 }
 
 function requestTick() {
-    if(!ticking) {
+    if (!ticking) {
         requestAnimationFrame(updateMovement);
     }
+
     ticking = true;
 }
 
@@ -61,6 +48,4 @@ function updateMovement() {
         var depth = moveEls[i].getAttribute('data-depth');
         moveEls[i].style.transform = "translate3d(0px, " + - scrollPosition/depth + "px, 0)";
     }
-
-
 }
